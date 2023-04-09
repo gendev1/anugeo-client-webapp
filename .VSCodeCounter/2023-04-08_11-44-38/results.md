4898dec2ed3f21af800440fe1c81f987e5f15780
Anirudh-1606 <anirudhsaladi7@gmail.com>
2023-04-09 11:57:24 +0530

    finishing it

---
+++ .VSCodeCounter/2023-04-08_11-44-38/results.md
@@ -0,0 +1,30 @@
+# Summary
+
+Date : 2023-04-08 11:44:38
+
+Directory c:\\Users\\Eswar\\Desktop\\Final\\client\\src
+
+Total : 20 files,  15779 codes, 3 comments, 96 blanks, all 15878 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| JavaScript | 17 | 15,739 | 3 | 92 | 15,834 |
+| CSS | 2 | 39 | 0 | 4 | 43 |
+| XML | 1 | 1 | 0 | 0 | 1 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 20 | 15,779 | 3 | 96 | 15,878 |
+| . (Files) | 5 | 263 | 0 | 26 | 289 |
+| components | 7 | 14,491 | 1 | 26 | 14,518 |
+| components (Files) | 3 | 207 | 1 | 14 | 222 |
+| components\\svgs | 4 | 14,284 | 0 | 12 | 14,296 |
+| pages | 8 | 1,025 | 2 | 44 | 1,071 |
+| pages (Files) | 3 | 487 | 0 | 20 | 507 |
+| pages\\dashboard | 5 | 538 | 2 | 24 | 564 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- src/pages/dashboard/Progress.js
+++ src/pages/dashboard/Progress.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import Accordion from "react-bootstrap/Accordion";
+
 import Sidebar from "../../components/Sidebar";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import {
 	Stepper,
 	Step,
@@ -24,7 +26,8 @@ const progressStep = {
 
 const Progress = ({ handleGetAllWorksByUser, currWorks, setCurrWorks }) => {
 	const { step, incrementStep, decrementStep } = useStepper(0, 4);
-	console.log(currWorks);
+	// const [latestWork, setLatestWork] = useState([]);
+	// const [remainingWorks, setRemainingWorks] = useState([]);
 	useEffect(() => {
 		handleGetAllWorksByUser();
 	}, []);
@@ -45,44 +48,155 @@ const Progress = ({ handleGetAllWorksByUser, currWorks, setCurrWorks }) => {
 			>
 				<DashboardNav />
 				<Container className="py-5">
-					{currWorks && (
-						<div className="info">
-							<p>Dheney progress page antaaru</p>
+					{currWorks.length && (
+						<Accordion defaultActiveKey={currWorks[0]._id}>
+							{currWorks.map((ele) => {
+								return (
+									<Accordion.Item eventKey={ele._id} key={ele._id}>
+										<Accordion.Header>
+											Work at {ele.address}
+											<br />
+											<br />
+											Placed order on:{" "}
+											{new Date(ele.createdAt).toLocaleDateString("en-GB")}
+										</Accordion.Header>
+										<Accordion.Body style={{ minHeight: "65vh" }}>
+											<div className="info">
+												<Stepper step={progressStep[ele.status]}>
+													<Step>
+														<StepNumber />
+														<StepTitle>
+															Awaiting work order confirmation
+														</StepTitle>
+														<StepStatus />
+														<StepDescription>
+															Your work needs to approved by the admin inorder
+															to initiate the work
+														</StepDescription>
+													</Step>
 
-							<Stepper step={4}>
-								<Step>
-									<StepNumber />
-									<StepTitle>Awaiting work order confirmation</StepTitle>
-									<StepStatus />
-									<StepDescription>
-										Your work needs to approved by the admin inorder to initiate
-										the work
-									</StepDescription>
-								</Step>
-								<Step>
-									<StepNumber />
-									<StepTitle>Field Work</StepTitle>
-									<StepStatus />
-									<StepDescription>
-										Sample collection at work site.
-									</StepDescription>
-								</Step>
-								<Step>
-									<StepNumber />
-									<StepTitle>Laboratory</StepTitle>
-									<StepStatus />
-									<StepDescription>
-										Soil testing reports at the lab
-									</StepDescription>
-								</Step>
-								<Step>
-									<StepNumber />
-									<StepTitle>Report Delivery</StepTitle>
-									<StepStatus />
-									<StepDescription>Report is delivered</StepDescription>
-								</Step>
-							</Stepper>
-						</div>
+													<Step>
+														<StepNumber />
+														<StepTitle>Field Work</StepTitle>
+														<StepStatus />
+														<StepDescription>
+															Sample collection at work site.
+														</StepDescription>
+													</Step>
+													<Step>
+														<StepNumber />
+														<StepTitle>Laboratory</StepTitle>
+														<StepStatus />
+														<StepDescription>
+															Soil testing reports at the lab
+														</StepDescription>
+													</Step>
+													<Step>
+														<StepNumber />
+														<StepTitle>Report Delivery</StepTitle>
+														<StepStatus />
+														<StepDescription>
+															Report is delivered
+														</StepDescription>
+													</Step>
+												</Stepper>
+											</div>
+											<br />
+											<br />
+											<br />
+											<br />
+											<br />
+											<br />
+											<br />
+											<br />
+											<br />
+											<div className="mt-5">
+												<Row>
+													<Col>
+														<div className="status">
+															<p>
+																Your work current status : <br />
+																<br />
+																<b
+																	className="mt-3"
+																	style={{
+																		backgroundColor: "#e7e9fd",
+																		padding: "5px 10px",
+																		color: "#3f42ed",
+																		borderRadius: "20px",
+																		fontSize: "16px",
+																	}}
+																>
+																	{ele.status}
+																</b>
+															</p>
+														</div>
+														<div className="dates pt-4">
+															<p>Your desired work commence date is: </p>
+															<b>
+																{new Date(ele.startDate).toLocaleDateString(
+																	"en-GB",
+																)}
+															</b>
+														</div>
+														<div className="service pt-4">
+															<p>Services opted for:</p>
+
+															<b>{ele.services}</b>
+														</div>
+													</Col>
+													<Col>
+														<div className="decription">
+															<p>Work Description:</p>
+
+															<p>{ele.description}</p>
+														</div>
+														<div className="pincode pt-4">
+															<p>Pin code of the area:</p>
+															<b>{ele.pinCode}</b>
+														</div>
+														{ele.bill && (
+															<div className="bill pt-2">
+																<p>Bill number:</p>
+																<b>{ele.bill}</b>
+															</div>
+														)}
+														{ele.workOrder && (
+															<div className="workOrder pt-2">
+																<p>Work order number:</p>
+																<b>{ele.workOrder}</b>
+															</div>
+														)}
+													</Col>
+													<Col>
+														<div className="address ">
+															<p>Address of the site:</p>
+															<p>{ele.address}</p>
+														</div>
+														{ele.endDate && (
+															<div className="address pt-2">
+																<p>Work completion date:</p>
+																<b>
+																	{new Date(ele.endDate).toLocaleDateString(
+																		"en-GB",
+																	)}
+																</b>
+															</div>
+														)}
+														{ele.workPayment && (
+															<div className="amount pt-2">
+																<p>Amount to pay</p>
+																<b>{ele.workPayment}</b>
+															</div>
+														)}
+													</Col>
+												</Row>
+											</div>
+										</Accordion.Body>
+									</Accordion.Item>
+								);
+							})}
+						</Accordion>
 					)}
 				</Container>
 			</div>
